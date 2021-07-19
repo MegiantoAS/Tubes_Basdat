0e88db433572473ff7985a483291ba4a909db679
MegiantoAS <adimegi85@gmail.com>
2021-07-19 14:22:31 +0700

    (Menambkan login)

---
+++ login.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet">
+    <title>Login</title>
+</head>
+
+<body style="background:linear-gradient(to right, #FFFFFF, #C6F2BF 80%)">
+
+    <div class="container mt-5">
+        <img src="./images/Group 11.png">
+        <div class="card mt-4 mb-4 " style="background:linear-gradient(to right, #FFFFFF, #C6F2BF)" style="box-shadow: black;">
+            <div class="row g-0 justify-content-center align-self-items h-100 w-100">
+                <div class="col-md-5">
+                    <img src="./images/login.png" width="350px" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-4">
+                    <div class="card-body">
+                        <form>
+                            <div class="sidebar-heading text-center">
+                                <h2 class="m-0 text-custom">LOGIN</h2>
+                                <img src="./images/Group 11.png"><br>
+                                Sign into your account
+                            </div>
+                            <div class="mb-1 mt-3">
+                                <label for="exampleInputEmail1" class="form-label "></label>
+                                <i class="fas fa-user-circle"></i>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Username">
+                                <div id="emailHelp" class="form-text">
+                                </div>
+                            </div>
+                            <div class="mb-2">
+                                <label for="exampleInputPassword1" class="form-label"></label>
+                                <i class="fas fa-lock"></i>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <button class="btn btn-success" type="button">Login</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footer-content mt" style="color:#BEBEBE;">
+            <p>copyright &copy;2021. MyLaundry. All Right Reserved </p>
+        </div>
+
+
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+        <!-- Option 2: Separate Popper and Bootstrap JS -->
+        <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+    -->
+</body>
+
+</html>
