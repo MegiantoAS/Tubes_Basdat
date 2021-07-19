0dcc5c276f4953a54b3d5b72ebca5d89b7121244
RidhwanAF <72844854+RidhwanAF@users.noreply.github.com>
2021-07-19 15:14:55 +0700

    Dashboard template
    dashboard dan style template (70% complete)

---
+++ dashboard.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="dashboard.css?v=1.5">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  
+</head>
+<body>
+    <div class="sidenav">
+        <label><a href="#"><img src="images/Group 11.png"></a></label>
+        <a href="#">Customer</a>
+        <a href="#">Pegawai</a>
+        <a href="#">Barang</a>
+        <a href="#">Cabang</a>
+        <a href="#">Pembayaran</a>
+        <button type="button" class="btn btn-danger col-1 mx-5">Logout</button>
+    </div>
+    
+    <div class="main">
+        <nav class="navbar navbar-expand-lg navbar-light bg-custom">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarText">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item">
+                <a class="nav-link" href="#">Profile</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+
+        ISI
+        
+    </div>
+    <div class="footer">
+        <div class="text-footer">Copyright © 2021. MyLaundry. All RIght Reserved</div>
+    </div>
+</body>
+</html> 
