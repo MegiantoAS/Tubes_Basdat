17489fdf07af9d94f7375dfd79fa14955197a38f
RidhwanAF <72844854+RidhwanAF@users.noreply.github.com>
2021-07-20 13:03:15 +0700

    dashboard v.1.0
    progress dashboard

--- dashboard.php
+++ dashboard.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="dashboard.css?v=1.5">
+    <link rel="stylesheet" href="dashboard.css?v=1.8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">  
 </head>
 <body>
@@ -13,7 +13,7 @@
         <a href="#">Barang</a>
         <a href="#">Cabang</a>
         <a href="#">Pembayaran</a>
-        <button type="button" class="btn btn-danger col-1 mx-5">Logout</button>
+        <button type="button" class="btn btn-danger mx-5 col-8">Logout</button>
     </div>
     
     <div class="main">
@@ -22,18 +22,35 @@
             <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                <a class="nav-link" href="#">Profile</a>
+                <a class="nav-link" href="#"><img src="images/iconprofile.png" width="30px"></a>
                 </li>
             </ul>
             </div>
         </div>
         </nav>
 
-        ISI
-        
+            <div class="box1">
+                Box 1 <br><img src="images/box1.png" width="300px">
+                <button type="button" class="btn btn-success col-2">Backup Data</button>
+            </div>
+            <div class="box2">
+                Box 2
+            </div>
+            <div class="box3">
+                Box 3
+            </div>
+            <div class="box4">
+                Box 4
+            </div>
+            <div class="box5">
+                Box 5
+            </div>
+            <div class="box6">
+                Box 6
+            </div>
     </div>
-    <div class="footer">
-        <div class="text-footer">Copyright © 2021. MyLaundry. All RIght Reserved</div>
-    </div>
+        <div class="text-footer">
+            Copyright © 2021. MyLaundry. All RIght Reserved
+        </div>
 </body>
 </html> 
