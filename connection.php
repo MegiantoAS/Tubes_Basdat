fff1f4da579aa392e6a49d314ed1f23fd559e16f
Dean Ghifari Faturrahman <dean12fatur@gmail.com>
2021-07-21 16:40:33 +0700

    add query count data
    -dashboard

--- connection.php
+++ connection.php
@@ -4,10 +4,9 @@ $databaseHost = 'localhost';
 $databaseName = 'laundry';
 $databaseUsername = 'root';
 $databasePassword = '';
- 
-$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
- 
-if($mysqli === false){
+
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName);
+
+if ($mysqli === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-?>
