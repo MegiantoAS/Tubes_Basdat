04acb05786eb814140e309e30eb5229c7249a2d2
Megianto Adi Saputra <72857078+MegiantoAS@users.noreply.github.com>
2021-07-28 23:05:39 +0700

    ubah dashboard dan css

--- dashboard.php
+++ dashboard.php
@@ -33,7 +33,7 @@ $dataPembayaran = mysqli_fetch_assoc($result);
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="dashboard.css?v=2.2">
+    <link rel="stylesheet" href="dashboard.css?v=2.3">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <title>MyLaundry | Dashboard</title>
 </head>
